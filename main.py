bb49cbd3b6af8b60967a0eb0de72c2602f40433e
Ihor <66976589+ihorlysenko-dev@users.noreply.github.com>
2025-05-12 15:08:54 +0200

    Changed translation a library, removed audio output

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord, os
 from discord.ext import commands
-from googletrans import Translator, LANGUAGES
+from deep_translator import GoogleTranslator
 from gtts import gTTS
 
 TOKEN = os.getenv("Discord_API")
@@ -18,21 +18,13 @@ async def on_ready():
 
 
 ### Languages list, separated into two messages due to discord's message limit of 2000 characters
-def lang_list():
-    text1_len = 0
-    text1 = ""
-    text2 = ""
-    for lang in LANGUAGES:
-        structure = f"{lang} - {LANGUAGES[lang]}\n"
-        if text1_len <= 1900:
-            text1 += structure
-            text1_len += len(structure)
-        else:
-            text2 += structure
-    return text1, text2
-
-
-text1, text2 = lang_list()  ### Initialize language a list once a program started
+def get_lang_list():
+    supported_languages = GoogleTranslator().get_supported_languages(as_dict=True)
+    lang_list = ""
+    for lang in supported_languages:
+        lang_list += f"{lang} - {supported_languages[lang]}\n"
+    return lang_list
+supported_languages_formated = get_lang_list()
 
 
 ### Generate random hash for file name
@@ -43,8 +35,7 @@ async def gen_hash():
 ### Command /languages
 @bot.tree.command(name='languages', description='List of supported languages')
 async def languages(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Languages supported: {text1}', ephemeral=True)
-    await interaction.followup.send(f'Languages supported: {text2}', ephemeral=True)
+    await interaction.response.send_message(f'Languages supported: \n{supported_languages_formated}', ephemeral=True)
 
 
 ### Command /translate
@@ -53,24 +44,16 @@ async def languages(interaction: discord.Interaction):
 @discord.app_commands.describe(destination='The language in "en/ja/ru" format', text='The text to translate',
                                from_lang='Manual source language input')
 async def translate(interaction: discord.Interaction, destination: str, text: str, from_lang: str = None):
-    translator = Translator()
+    ### Translate text
     if from_lang is None:  ### Detecting manual language input
-        translation = await translator.translate(text, dest=destination)
+        translation = GoogleTranslator(source='auto', target=destination).translate(text=text)
     else:
-        translation = await translator.translate(text, dest=destination, src=from_lang)
+        translation = GoogleTranslator(source=from_lang, target=destination).translate(text=text)
 
     ### Constructing a message to send
-    text_to_send = (f"From {translation.src.upper()} to {translation.dest.upper()}\n"
-                    f"Text: {text}\n"
-                    f"Translation: {translation.text}\n"
-                    f"Pronunciation: {translation.pronunciation}")
+    await interaction.response.send_message(f"Translation: {translation}", ephemeral=True)
 
-    ### Generating and sending a message with a file, then deleting a file
-    file_hash = await gen_hash()
-    tts = gTTS(translation.text, lang=translation.dest)
-    tts.save(f'files/{file_hash}.mp3')
-    await interaction.response.send_message(text_to_send, ephemeral=False, file=discord.File(f'files/{file_hash}.mp3'))
-    os.remove(f'files/{file_hash}.mp3')
+
 
 
 if __name__ == '__main__':
