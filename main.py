270e6abf72ec2d09a1358302e84af37a40dd6f29
Ihor <66976589+ihorlysenko-dev@users.noreply.github.com>
2025-05-12 22:56:54 +0200

    * Added .json database for users with self-initialization when bot starts
    * Reading and Writing to the database now separated functions and they are async
    
    * Added LICENSE and changed .gitignore
    
    * Other minor changes

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-import discord, os
+import discord, os, json
 from discord.ext import commands
 from deep_translator import GoogleTranslator
-from usersdata import users_database
 
 TOKEN = os.getenv("Discord_API")
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
+
+### Database initialization
+def database_init():
+    try:
+        with open("userdata.json"):
+            print("Database successfully loaded")
+    except FileNotFoundError:
+        with open("userdata.json", "w") as f:
+            json.dump([], f)
+
+
+### Database read and write
+async def read_database():
+    with open("userdata.json") as f:
+        return json.load(f)
+
+
+async def write_database(data):
+    with open("userdata.json", "w") as f:
+        json.dump(data, f)
 
 
 @bot.event
@@ -39,11 +58,6 @@ def get_lang_list():
 supported_languages_list = get_lang_list()
 
 
-### Generate random hash for file name
-async def gen_hash():
-    return os.urandom(16).hex()
-
-
 ### Command /languages
 @bot.tree.command(name='languages', description='List of supported languages')
 async def languages(interaction: discord.Interaction):
@@ -51,9 +65,10 @@ async def languages(interaction: discord.Interaction):
 
 
 ### Command /translate
-@bot.tree.command(name='translate', description='Autodetect language and translate to chosen language,'
-                                                ' or manually input language')
-@discord.app_commands.describe(destination='The language in "en/ja/ru" format', text='The text to translate',
+@bot.tree.command(name='translate',
+                  description='Autodetect language and translate to chosen language, or manually input language')
+@discord.app_commands.describe(destination='The language in "en/ja/ru" format',
+                               text='The text to translate',
                                from_lang='Manual source language input')
 async def translate(interaction: discord.Interaction, destination: str, text: str, from_lang: str = None):
     ### Translate text
@@ -69,7 +84,7 @@ async def translate(interaction: discord.Interaction, destination: str, text: st
 ### Context menu button Translate
 @bot.tree.context_menu(name='Translate')
 async def translate_context(interaction: discord.Interaction, message: discord.Message):
-    dest_lang = await get_user_language(interaction.user.id)
+    dest_lang = await get_user_language(str(interaction.user.id))
     if dest_lang:
         translation = GoogleTranslator(target=f"{dest_lang}").translate(text=message.content)
         await interaction.response.send_message(f"Translation:\n{translation}", ephemeral=True)
@@ -79,29 +94,42 @@ async def translate_context(interaction: discord.Interaction, message: discord.M
 
 
 ### Get default language for context menu button Translate from database
-async def get_user_language(interaction_user_id: int):
-    for user in users_database:
+async def get_user_language(interaction_user_id: str):
+    users_db = await read_database()
+    for user in users_db:
         if interaction_user_id in user:
             return user[interaction_user_id]
     return None
 
 
-@bot.tree.command(name='set_language', description='Sets your default language this will be used ONLY for Translate'
-                                                   ' context menu button')
+@bot.tree.command(name='set_language',
+                  description='Sets your default language this will be used ONLY for Translate context menu button')
 @discord.app_commands.describe(destination='The language in "en/ja/ru" format, for more detail use /languages command')
 async def set_language(interaction: discord.Interaction, destination: str):
+    ### Checking if the language is supported
     if destination not in supported_languages_list:
-        await interaction.response.send_message("This language is not supported", ephemeral=True)
+        await interaction.response.send_message("This language is not supported\n"
+                                                "Try again", ephemeral=True)
 
+    ### Checking if the user is already in the database
     else:
-        user_id = interaction.user.id
-        for user in users_database:
+        ### Updating database
+        user_id = str(interaction.user.id)  # converting user_id to string
+        users_db = await read_database()
+        ### If the user is already in a database, update his language
+        for user in users_db:
             if user_id in user:
                 user[user_id] = destination
+                await write_database(users_db)
                 await interaction.response.send_message(f"Language set to {destination}", ephemeral=True)
-        users_database.append({user_id: destination})
-        await interaction.response.send_message(f"Language set to {destination}", ephemeral=True)
+                break
+        else:
+            ### If the user is not in a database, add him
+            users_db.append({user_id: destination})
+            await write_database(users_db)
+            await interaction.response.send_message(f"Language set to {destination}", ephemeral=True)
 
 
 if __name__ == '__main__':
+    database_init()
     bot.run(TOKEN)
