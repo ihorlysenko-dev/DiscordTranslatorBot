501691ab5b1d1b402ace63cdbcf85d0d68986753
Ihor <66976589+ihorlysenko-dev@users.noreply.github.com>
2025-05-12 21:02:14 +0200

    * Added context menu translation function
    * Added command /set_language to add the default user language (works only with a context menu)
    
    * Updated /languages with new supported languages

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord, os
 from discord.ext import commands
 from deep_translator import GoogleTranslator
-from gtts import gTTS
+from usersdata import users_database
 
 TOKEN = os.getenv("Discord_API")
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
 
 
 @bot.event
@@ -17,14 +18,25 @@ async def on_ready():
         print(e)
 
 
-### Languages list, separated into two messages due to discord's message limit of 2000 characters
-def get_lang_list():
+### Supported languages as text
+def get_lang_text():
     supported_languages = GoogleTranslator().get_supported_languages(as_dict=True)
     lang_list = ""
     for lang in supported_languages:
         lang_list += f"{lang} - {supported_languages[lang]}\n"
     return lang_list
-supported_languages_formated = get_lang_list()
+
+
+supported_languages_formated = get_lang_text()
+
+
+### Supported languages as a list
+def get_lang_list():
+    supported_languages = GoogleTranslator().get_supported_languages(as_dict=True)
+    return [lang for lang in supported_languages.values()]
+
+
+supported_languages_list = get_lang_list()
 
 
 ### Generate random hash for file name
@@ -46,7 +58,7 @@ async def languages(interaction: discord.Interaction):
 async def translate(interaction: discord.Interaction, destination: str, text: str, from_lang: str = None):
     ### Translate text
     if from_lang is None:  ### Detecting manual language input
-        translation = GoogleTranslator(source='auto', target=destination).translate(text=text)
+        translation = GoogleTranslator(target=destination).translate(text=text)
     else:
         translation = GoogleTranslator(source=from_lang, target=destination).translate(text=text)
 
@@ -54,6 +66,41 @@ async def translate(interaction: discord.Interaction, destination: str, text: st
     await interaction.response.send_message(f"Translation: {translation}", ephemeral=True)
 
 
+### Context menu button Translate
+@bot.tree.context_menu(name='Translate')
+async def translate_context(interaction: discord.Interaction, message: discord.Message):
+    dest_lang = await get_user_language(interaction.user.id)
+    if dest_lang:
+        translation = GoogleTranslator(target=f"{dest_lang}").translate(text=message.content)
+        await interaction.response.send_message(f"Translation:\n{translation}", ephemeral=True)
+    else:
+        await interaction.response.send_message("You have not set a default language yet\n"
+                                                "Please use /set_language command for it", ephemeral=True)
+
+
+### Get default language for context menu button Translate from database
+async def get_user_language(interaction_user_id: int):
+    for user in users_database:
+        if interaction_user_id in user:
+            return user[interaction_user_id]
+    return None
+
+
+@bot.tree.command(name='set_language', description='Sets your default language this will be used ONLY for Translate'
+                                                   ' context menu button')
+@discord.app_commands.describe(destination='The language in "en/ja/ru" format, for more detail use /languages command')
+async def set_language(interaction: discord.Interaction, destination: str):
+    if destination not in supported_languages_list:
+        await interaction.response.send_message("This language is not supported", ephemeral=True)
+
+    else:
+        user_id = interaction.user.id
+        for user in users_database:
+            if user_id in user:
+                user[user_id] = destination
+                await interaction.response.send_message(f"Language set to {destination}", ephemeral=True)
+        users_database.append({user_id: destination})
+        await interaction.response.send_message(f"Language set to {destination}", ephemeral=True)
 
 
 if __name__ == '__main__':
